b68975028c0c89eb34f2aea09175b55aae0df576
devtricks-company <amir.mo.azarbashi@gmail.com>
2020-08-23 00:44:06 +0430

    added posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,21 +11,21 @@ module.exports = {
       resolve:`gatsby-source-filesystem`,
       options:{
         name:"pages",
-        path:`${__dirname}/pages`
+        path:`${__dirname}/src/pages`
       }
     },
     {
       resolve:`gatsby-source-filesystem`,
       options:{
         name:"posts",
-        path:`${__dirname}/posts`
+        path:`${__dirname}/src/posts`
       }
     },
     {
       resolve:`gatsby-source-filesystem`,
       options:{
         name:"images",
-        path:`${__dirname}/images`
+        path:`${__dirname}/src/images`
       },
      
     },
